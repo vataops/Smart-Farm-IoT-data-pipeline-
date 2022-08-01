5580af8b02d7a9131869782d56d1c40055e62dbb
yeeeseo <cen023@naver.com>
2022-08-01 17:23:40 +0900

    Change: parquet 파일 읽기 구현

--- validate-lambda/handler.py
+++ validate-lambda/handler.py
@@ -2,19 +2,27 @@ import json
 import urllib.parse
 import boto3
 import pandas
-s3 = boto3.client('s3')
+import pyarrow.parquet as pq
+import io
 
 def hello(event, context):
-    print("Received event: " + json.dumps(event))
+    # print("Received event: " + json.dumps(event))
 
-    # Get the object from the event and show its content type
+    buffer = io.BytesIO()
+    s3 = boto3.resource('s3')
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    key = event['Records'][0]['s3']['object']['key']
+    # print('bucketbucket: ' + bucket)
+    # print('keykey: ' + key)
     try:
-        response = s3.get_object(Bucket=bucket, Key=key)
-        df = pandas.read_parquet("s3://{0}/{1}".format(bucket,key), engine='auto', columns=None, storage_options=None, use_nullable_dtypes=False, **kwargs)
+        # if문 추가 : 비정상적인 데이터 정제
+        # 비정상적인 데이터 검출 시, 다른 파일로 이어지게..?
+        s3_object = s3.Object(bucket, key)
+        s3_object.download_fileobj(buffer)
+        table = pq.read_table(buffer)
+        df = table.to_pandas()
         print(df.head(1))
-        return response['ContentType']
+        return "Hello"
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
