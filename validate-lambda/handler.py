e26884bcf7362f79a08d589999e444bc19874398
yeeeseo <cen023@naver.com>
2022-08-04 16:27:37 +0900

    Add: Discord Webhook 기능 구현

--- validate-lambda/handler.py
+++ validate-lambda/handler.py
@@ -5,14 +5,16 @@ import pandas as pd
 import pyarrow.parquet as pq
 import io
 import time
-from time import localtime
-from time import strftime
 import os
 import datetime
 
-def hello(event, context):
-    # print("Received event: " + json.dumps(event))
+from time import localtime
+from time import strftime
+from base64 import b64decode
+from urllib.request import Request, urlopen
 
+
+def hello(event, context):
     buffer = io.BytesIO() # buffer 저장공간 확보
     s3 = boto3.resource('s3')
     bucket = event['Records'][0]['s3']['bucket']['name']
@@ -21,10 +23,13 @@ def hello(event, context):
     korea_time = datetime.datetime.now() + datetime.timedelta(hours = 9)
     dest_s3_name = os.environ.get('DEST_S3_NAME')
     file_name = korea_time.strftime('%Y-%m-%d-%I:%M:%S%p.parquet')
+    HOOK_URL = os.environ.get('HOOK_URL')
 
-
-    # print('bucketbucket: ' + bucket)
-    # print('keykey: ' + key)
+    discord_message = {
+        'username': 'test',
+        'content': 'Test'
+    }
+    
     try:
         s3_object = s3.Object(bucket, key)
         s3_object.download_fileobj(buffer) # s3에서 받아온 데이터를 buffer에 파일 형태로 저장
@@ -33,26 +38,79 @@ def hello(event, context):
 
         # df의 데이터를 조건에 맞게 쿼리
         err = df[(df.error_code == 1)]
-        temp = df[(df.temperature <= 17) | (df.temperature >= 28)]
-        hum = df[(df.humidity <= 59) | (df.humidity >= 81)]
+        temp = df[(df.temperature <= 18) | (df.temperature >= 27)]
+        hum = df[(df.humidity <= 60) | (df.humidity >= 80)]
         co2 = df[(df.co2 <= 650) | (df.co2 >= 750)]
 
-        # 쿼리한 데이터를 모으고, parquet 파일로 변환
-        filtered_df = pd.concat([err, temp, hum, co2]).drop_duplicates(subset=["server_time"], ignore_index=True)
-        os.chdir('/tmp')
-        os.makedirs('dest_s3', exist_ok= True)
-        os.chdir('dest_s3')
-        result = filtered_df.to_parquet(file_name)
-        print('변환 성공')
+        # 쿼리한 데이터를 모으기
+        filtered_df = pd.concat([err, temp, hum, co2]).drop_duplicates(subset=["server_time", "device_id"], ignore_index=True)
 
-        # dest_s3에 파일 저장
-        # file = io.BytesIO(bytes(result), encoding = 'utf-8')
-        dest_s3 = s3.Bucket(dest_s3_name)
-        # bucket_object = dest_s3.Object(file_name)
-        dest_s3.upload_file('/tmp/dest_s3/{}'.format(file_name), file_name)
-        print('성공')
+        # 하나라도 있으면 discord webhook 전송
+        if len(filtered_df) != 0 :
+            unique_dev_ids = filtered_df.device_id.unique()
 
-        return 'success'
+            filtered_err = filtered_df[(filtered_df.error_code == 1)]
+            
+            time_arr = filtered_df.server_time.to_numpy()
+            device_id_arr = filtered_df.device_id.to_numpy()
+            temp_arr = filtered_df.temperature.to_numpy()
+            pres_arr = filtered_df.pressure.to_numpy()
+            hum_arr = filtered_df.humidity.to_numpy()
+            co2_arr = filtered_df.co2.to_numpy()
+
+            message_arr = []
+            if len(filtered_err) != 0:
+                for dev_id in unique_dev_ids:
+                    dev_id_df = filtered_df[filtered_df.device_id == dev_id]
+                    for i in range(len(dev_id_df)) :
+                        message = 'device id : {}번 센서가 아파요'.format(device_id_arr[i])
+                        message_arr.append(message)
+                    result = " ".join(message_arr)
+                    discord_message = {
+                        'username': 'test',
+                        'content': result
+                    }
+            else: 
+                for dev_id in unique_dev_ids:
+                    dev_id_df = filtered_df[filtered_df.device_id == dev_id]
+                    for i in  range(len(dev_id_df)):
+                        message = '{} \n device {} 에서 이상 데이터가 감지됨 \n temperature : {}\u00B0 \n pressure : {}hPa \n humidity : {}% \n co2 : {}ppm \n --- \n'.format(time_arr[i], device_id_arr[i], temp_arr[i],pres_arr[i], hum_arr[i], co2_arr[i])
+                        message_arr.append(message)
+                    result = " ".join(message_arr)
+                    discord_message = {
+                        'username': 'test',
+                        'content': result
+                    }
+
+            payload = json.dumps(discord_message).encode('utf-8')
+            headers = {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Content-Length': len(payload),
+                'Host': 'discord.com',
+                'user-agent': 'Mozilla/5.0'
+            }
+
+            req = Request(HOOK_URL, payload, headers)
+            response = urlopen(req)
+            response.read()
+
+            # parquet 파일로 변환
+            os.chdir('/tmp')
+            os.makedirs('dest_s3', exist_ok= True)
+            os.chdir('dest_s3')
+            result = filtered_df.to_parquet(file_name)
+            print('변환 성공')
+
+            # dest_s3에 파일 저장
+            # file = io.BytesIO(bytes(result), encoding = 'utf-8')
+            dest_s3 = s3.Bucket(dest_s3_name)
+            # bucket_object = dest_s3.Object(file_name)
+            dest_s3.upload_file('/tmp/dest_s3/{}'.format(file_name), file_name)
+            print('S3에 저장 성공')
+
+        print('이상 데이터 없음')
+        return 'Process Succeeded'
+
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
