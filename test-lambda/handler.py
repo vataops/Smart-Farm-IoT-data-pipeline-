f3d5f62ca858020f3fc1270a7865b22c1065f149
tigercoin-world <shsfse@naver.com>
2022-08-04 13:38:18 +0900

    Change: handler.py device_id 추가

--- test-lambda/handler.py
+++ test-lambda/handler.py
@@ -53,20 +53,44 @@ def hello(event, context):
 
     i = 0
 
+    device_num = 2;
+
     while i < count:
-        err_randint = randint(0, 4)
-        if err_randint == 0:
-            err = 1
+        err_randint_1 = randint(0, 4)
+        err_randint_2 = randint(0, 5)
+
+        if err_randint_1 == 0:
+            err_1 = 1
         else:
-            err = 0
-        devi = '39278391'
+            err_1 = 0
+        if err_randint_2 == 0:
+            err_2 = 1
+        else:
+            err_2 = 0
+        
+        devi_1 = '39278391'
+        devi_2 = '51539982'
+
         temp = randint(13, 35)
         humi = randint(50, 90)
         pres = randint(750, 1500)
         co2 = randint(675, 825)
         timestamp = time.time()
 
-        result = put_to_api(temp, humi, co2, pres, timestamp , devi, err)
-        print('response: {}'.format(result))
+        result_1 = put_to_api(temp, humi, co2, pres, timestamp , devi_1, err_1)
+        print('response: {}'.format(result_1))
+
+        result_2 = put_to_api(temp, humi, co2, pres, timestamp, devi_2, err_2)
+        print('response: {}'.format(result_2))
         time.sleep(inter)
+
+
         i = i + 1
+
+    message = {'테스트 device 수': device_num, '총 실행시간': count*inter}
+
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(message)
+    }
