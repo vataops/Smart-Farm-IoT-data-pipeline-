72566d1b233aa45631d36f96275a4d3ee4fc1d9d
DongwooHONG <hdongwoo1479@gmail.com>
2022-08-10 18:32:24 +0900

    Change: minor logic fix

--- test.py
+++ test.py
@@ -67,16 +67,20 @@ while i < count:
     devi_1 = ['39278391', '34', '-118.24']
     devi_2 = ['51539982', '37.3', '-122']
 
-    temp = randint(13, 35)
-    humi = randint(50, 90)
-    pres = randint(750, 1500)
-    co2 = randint(675, 825)
+    temp1 = randint(13, 35)
+    temp2 = randint(13, 35)
+    humi1 = randint(50, 90)
+    humi2 = randint(50, 90)
+    pres1 = randint(750, 1500)
+    pres2 = randint(750, 1500)
+    co21 = randint(675, 825)
+    co22 = randint(675, 825)
     timestamp = time.time()
 
-    result_1 = put_to_api(temp, humi, co2, pres, timestamp, devi_1[0], err_1, devi_1[2], devi_1[1])
+    result_1 = put_to_api(temp1, humi1, co21, pres1, timestamp, devi_1[0], err_1, devi_1[2], devi_1[1])
     print('response: {}'.format(result_1))
 
-    result_2 = put_to_api(temp, humi, co2, pres, timestamp, devi_2[0], err_2, devi_2[2], devi_2[1])
+    result_2 = put_to_api(temp2, humi2, co22, pres2, timestamp, devi_2[0], err_2, devi_2[2], devi_2[1])
     print('response: {}'.format(result_2))
     time.sleep(inter)
 
