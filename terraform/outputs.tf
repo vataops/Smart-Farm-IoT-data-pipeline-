786bc907b817fcc0e539c6d02c2d4657226e0184
DongwooHONG <hdongwoo1479@gmail.com>
2022-08-11 11:16:07 +0900

    Change: output 값 추가

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,13 @@ output "pseudo-api-gw-endpoint" {
     value = "${aws_api_gateway_stage.test-s1.invoke_url}/resource"
     description = "Endpoint for pseudo-api-gw resource"
 }
+
+output "grafana-ec2-public-dns" {
+    value = "http://${aws_instance.grafana-ec2-final.public_dns}:3000"
+    description = "Public IPv4 DNS for EC2 instance running Grafana"
+}
+
+output "grafana-public-domain-address" {
+    value = "${aws_route53_record.www.name}:3000"
+    description = "Public domain address for Grafana service"
+}
